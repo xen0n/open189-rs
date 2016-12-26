735c8350ae17e014cb980fc43ab3c906c8e55e2f
Wang Xuerui <git@xen0n.name>
2016-12-26 20:42:24 +0800

    init: barebones

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+#![feature(proc_macro)]
+#![recursion_limit = "1024"]
+
+extern crate chrono;
+#[macro_use]
+extern crate error_chain;
+extern crate hyper;
+#[macro_use]
+extern crate lazy_static;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
