bc64738559f26bee7c95da02956cc3be6bca9eeb
Wang Xuerui <git@xen0n.name>
2016-12-26 21:20:52 +0800

    init(util): port state string generation from pyopen189

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,8 @@ extern crate error_chain;
 extern crate hyper;
 #[macro_use]
 extern crate lazy_static;
+extern crate rand;
+extern crate rustc_serialize;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,11 @@
 use chrono::*;
+use rand;
+use rand::Rng;
+use rustc_serialize::base64;
+use rustc_serialize::base64::ToBase64;
 
 const UTC_8_OFFSET: i32 = 8 * 3600;
+const RANDOM_STATE_LENGTH: usize = 30;
 
 lazy_static! {
     static ref ASIA_SHANGHAI: FixedOffset = FixedOffset::east(UTC_8_OFFSET);
@@ -12,4 +17,23 @@ pub fn get_api_timestamp() -> String {
     let utc_time = UTC::now();
     let time = utc_time.with_timezone(&*ASIA_SHANGHAI);
     time.format("%Y-%m-%d %H:%M:%S").to_string()
+}
+
+
+/// Generates a sufficiently long random string for tracking OAuth requests.
+pub fn get_random_state_str() -> String {
+    let mut rng = rand::thread_rng();
+    let bytes: Vec<_> = rng.gen_iter::<u8>().take(RANDOM_STATE_LENGTH).collect();
+    b64encode(&bytes)
+}
+
+
+/// Base64-encodes the given buffer.
+pub fn b64encode(input: &[u8]) -> String {
+    input.to_base64(base64::Config {
+        char_set: base64::CharacterSet::Standard,
+        newline: base64::Newline::LF,
+        pad: true,
+        line_length: None,
+    })
 }
